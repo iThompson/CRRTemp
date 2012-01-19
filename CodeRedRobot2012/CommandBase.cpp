b38ee7113863ba4d1979c6114de055e4b6a95f8f
Ian Thompson <thepi200@gmail.com>
2012-01-18 23:50:08 -0500

    Initial import of 2012 Robot project. GO RED!

---
+++ CodeRedRobot2012/CommandBase.cpp
@@ -0,0 +1,18 @@
+#include "CommandBase.h"
+#include "Commands/Scheduler.h"
+
+CommandBase::CommandBase(const char *name) : Command(name) {
+}
+
+CommandBase::CommandBase() : Command() {
+}
+
+// Initialize a single static instance of all of your subsystems to NULL
+OI* CommandBase::oi = NULL;
+
+void CommandBase::init() {
+    // Create a single static instance of all of your subsystems. The following
+	// line should be repeated for each subsystem in the project.
+	
+	oi = new OI();
+}
