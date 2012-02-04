832908c48ab25cb47dace70678ce3890661ad3fc
christoph.evans <christoph.evans@hse16-imacw-10.icsd.k12.ny.us>
2012-02-04 16:34:13 -0500

    Same as begore, now done correctly

---
+++ CodeRedRobot2012/Commands/JoustickHalfDrive.cpp
@@ -0,0 +1,39 @@
+
+#include "JoystickHalfDrive.h"
+
+JoystickHalfDrive::JoystickHalfDrive() : CommandBase( "JoystickHalfDrive" ) {
+	Requires(drive);
+	
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(chassis);
+}
+
+// Called just before this Command runs the first time
+void JoystickHalfDrive::Initialize() {
+	
+}
+
+// Called repeatedly when this Command is scheduled to run
+void JoystickHalfDrive::Execute() {
+	double l, r;
+	l = oi->GetYLeft();
+	l = l / 2;
+	r = oi->GetYRight();
+	r = r / 2;
+	drive->TankDrive(l, r);
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool JoystickHalfDrive::IsFinished() {
+	return false;
+}
+
+// Called once after isFinished returns true
+void JoystickHalfDrive::End() {
+	
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void JoystickHalfDrive::Interrupted() {
+}
---
+++ CodeRedRobot2012/Commands/JoystickHalfDrive.h
@@ -0,0 +1,21 @@
+#ifndef JOYSTICKHALFDRIVE_H
+#define JOYSTICKHALFDRIVE_H
+
+#include "../CommandBase.h"
+
+/**
+ *
+ *
+ * @author Chris Evans
+ */
+class JoystickHalfDrive: public CommandBase {
+public:
+	JoystickHalfDrive();		//Declare functions for the class JoystickDrive()
+	virtual void Initialize();
+	virtual void Execute();
+	virtual bool IsFinished();
+	virtual void End();
+	virtual void Interrupted();
+};
+
+#endif
