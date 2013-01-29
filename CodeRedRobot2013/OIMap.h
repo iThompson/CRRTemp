ef7b5f38e0df772c032c39328cad280f9033dfe5
Ian Programmer <ijt@twcny.rr.com>
2013-01-29 18:32:58 -0500

    Created OIMap.h, which may be of limited use, depending on how heavily we rely on robotbuilder.

---
+++ CodeRedRobot2013/OIMap.h
@@ -0,0 +1,26 @@
+#ifndef OIMAP_H
+#define OIMAP_H
+
+/* Naming scheme for ports:
+ * "Subsystem_Type_Function"
+ * 
+ * Subsystem:
+ * GTE - Gate
+ * DRV - Drive
+ * LDR - Loader
+ * RBT - No subsystem
+ * SHO - Shooter
+ * 
+ * Type:
+ * JOY - Joystick
+ * ANA - Analog input
+ * DIN - Digital input
+ * DOT - Digital output
+ * 
+ */
+
+//Drive
+#define DRV_JOY_LEFT 1
+#define DRV_JOY_RIGHT 2
+
+#endif
