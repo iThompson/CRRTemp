5dc2b2fbcef80275eeb24aa998cbffa6d6678afa
CRR Programmer <CRRProgrammer@gmail.com>
2015-01-29 18:52:39 -0500

    In process of setting up tote stacker positions.

---
+++ CodeRedRobot2015/src/Subsystems/StackerRelative.h
@@ -0,0 +1,19 @@
+/*
+ * StackerPositions.h
+ *
+ *  Created on: Jan 29, 2015
+ *      Author: Programmer
+ */
+
+#ifndef SRC_SUBSYSTEMS_STACKERRELATIVE_H_
+#define SRC_SUBSYSTEMS_STACKERRELATIVE_H_
+
+enum StackerRelative {
+	GROUND = 0, //Dummy Value for ground
+	SCORING_PLAT = 1, //Dummy Value for scoring platform
+	STEP = 2 //Dummy Value for step
+};
+
+
+
+#endif /* SRC_SUBSYSTEMS_STACKERPOSITIONS_H_ */
