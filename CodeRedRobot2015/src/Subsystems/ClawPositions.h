cfc8db0ab18740227b25d87b7f3991b5478b4f38
CRR Programmer <CRRProgrammer@gmail.com>
2015-01-31 11:57:44 -0500

    Looks like Container Stacker is done. Might need some changes but I can do those later.

---
+++ CodeRedRobot2015/src/Subsystems/ClawPositions.h
@@ -0,0 +1,24 @@
+/*
+ * ClawPositions.h
+ *
+ *  Created on: Jan 31, 2015
+ *      Author: Programmer
+ */
+
+#ifndef SRC_SUBSYSTEMS_CLAWPOSITIONS_H_
+#define SRC_SUBSYSTEMS_CLAWPOSITIONS_H_
+
+enum ClawPositions {
+	PICK_STANDING = 0, //TODO: Dummy Value Replace
+	PICK_FALL = 1, //TODO: Dummy Value Replace
+	LEVEL_1 = 2, //TODO: Dummy Value Replace
+	LEVEL_2 = 3, //TODO: Dummy Value Replace
+	LEVEL_3 = 4, //TODO: Dummy Value Replace
+	LEVEL_4 = 5, //TODO: Dummy Value Replace
+	LEVEL_5 = 6, //TODO: Dummy Value Replace
+	MAX = 7 //TODO: Dummy Value Replace
+};
+
+
+
+#endif /* SRC_SUBSYSTEMS_CLAWPOSITIONS_H_ */
