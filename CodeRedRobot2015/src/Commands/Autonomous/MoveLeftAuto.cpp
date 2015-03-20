98a13e91ab093ba481174b87c561784071db61c5
CRR Programmer <CRRProgrammer@gmail.com>
2015-03-20 11:05:52 -0400

    Match ready code. Autonomous kinda works, moves right.

--- CodeRedRobot2015/src/Commands/Autonomous/MoveLeftAuto.cpp
+++ CodeRedRobot2015/src/Commands/Autonomous/MoveLeftAuto.cpp
@@ -8,6 +8,6 @@ MoveLeftAuto::MoveLeftAuto() {
 	AddParallel(new ResetToteNew(), 10);
 	AddParallel(new ResetClaw());
 	AddSequential(new LowerHWheel());
-	AddSequential(new JoystickAutoDrive(0,.5,0,0,60 * INCH,0), 2);
+	AddSequential(new JoystickAutoDrive(0,.75,0,0,60 * INCH,0), 4);
 	AddSequential(new JoystickAutoDrive(0,0,0,0,0,0),2.5);
 }
